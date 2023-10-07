0bb9c7c5c2ad80f44e0ff105e494cd3c89536387
Panos-Stergioulas-Mpolis <panosstergioulasmpolis@gmail.com>
2023-10-07 16:08:37 +0300

    Min heap remive smallest index

--- Heaps/MinHeap.js
+++ Heaps/MinHeap.js
@@ -26,6 +26,51 @@ class MinHeap{
         }
     }
 
+    Remove(){
+        let smallest = this.heap[1];
+        if(this.heap.length > 2){
+            this.heap[1] = this.heap[this.heap.length - 1];
+            this.heap.splice(this.heap.length -1)
+            if(this.heap.length === 3){
+                if(this.heap[1] > this.heap[2]){
+                    let temp = this.heap[1];
+                    this.heap[1] = this.heap[2];
+                    this.heap[2] = temp;
+                }
+                return smallest;
+            }
+            let i = 1;
+            let left = 2 * i;
+            let right = 2 * i + 1;
+            while(this.heap[i] >= this.heap[left] || this.heap[i] >= this.heap[right]){
+                if(this.heap[left] <= this.heap[right]){
+                    let temp = this.heap[i];
+                    this.heap[i] = this.heap[left];
+                    this.heap[left] = temp;
+                    i = 2 * i;
+                }
+                else{
+                    let temp = this.heap[i];
+                    this.heap[i] = this.heap[right];
+                    this.heap[right] = temp;
+                    i = 2 * i + 1;
+                }
+                left = 2 * i;
+                right = 2 * i + 1;
+                if(this.heap[left] === undefined || this.heap[right] === undefined){
+                    break;
+                }
+            }
+        }
+        else if(this.heap.length === 2){
+            this.heap.splice(1,1);
+        }
+        else{
+            return null;
+        }
+        return smallest;
+    }
+
 }
 
 const mh = new MinHeap();
@@ -40,4 +85,6 @@ mh.Insert(5);
 mh.Insert(3);
 mh.Insert(16);
 mh.Insert(2);
+mh.Remove();
+mh.Remove();
 console.log(mh);
