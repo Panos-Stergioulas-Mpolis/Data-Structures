290b809ae75ff870c5b8ada1caec0dae0d515054
Panos-Stergioulas-Mpolis <panosstergioulasmpolis@gmail.com>
2023-10-09 13:32:19 +0300

    Leven order traversal (BTS traversal)

--- Binary Search Tree/BinarySearchTree.js
+++ Binary Search Tree/BinarySearchTree.js
@@ -7,6 +7,56 @@ class Node{
     }
 }
 
+class QueueNode{
+    constructor(value, next = null, prev = null){
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+
+class Queue{
+    constructor(){
+        this.front = null;
+        this.back = null;
+        this.size = 0;
+    }
+
+    // Return the size of the queue
+    GetSize(){
+        return this.size;
+    }
+
+    // Adds an element at the back of the queue
+    Insert(data){
+        if(this.size === 0){
+            this.front = this.back = new QueueNode(data);
+            this.size++;
+        }
+        else{
+            this.back.next = new QueueNode(data, null, this.back);
+            this.back = this.back.next;
+            this.size++;
+        }
+    }
+
+    // Removes an element from the dront of the queue
+    Remove(){
+        if(this.size === 0){
+            console.log("The queue is empty");
+        }
+        else if(this.size === 1){
+            this.front = this.back = null;
+            this.size--;
+        }
+        else{
+            this.front = this.front.next;
+            this.front.prev = null;
+            this.size--;
+        }
+    }
+}
+
 class BinarySearchTree{
     constructor(){
         this.root = null;
@@ -45,27 +95,51 @@ class BinarySearchTree{
         }
     }
 
-    // Remove(value){
-    //     let trav = this.root;
-    //     while(trav !== null){
-    //         if(trav.value === value){
-    //             if(trav.left === null && trav.right === null){
-    //                 trav = null;
-    //                 this.numOfNodes--;
-    //                 break;
-    //             }
-    //             else if(trav.left === null){
-    //                 trav.parent.right = trav.right;
-    //                 trav.right.parent = trav.parent;
-    //                 trav = null;
-    //                 this.numOfNodes--;
-    //             }
-    //             else if(trav.right === null){
+    Remove(value){
+        let trav = this.root;
+        while(trav !== null){
+            if(trav.value === value){
+                if(trav.left === null && trav.right === null){
+                    trav = null;
+                    this.numOfNodes--;
+                    break;
+                }
+                else if(trav.left === null){
+                    trav.parent.right = trav.right;
+                    trav.right.parent = trav.parent;
+                    trav = null;
+                    this.numOfNodes--;
+                }
+                else if(trav.right === null){
 
-    //             }
-    //         }
-    //     }
-    // }
+                }
+            }
+        }
+    }
+
+    // BFS
+    LevelorderPrint(){
+        let trav = this.root;
+        const queue = new Queue();
+        if(this.numOfNodes != 0){
+            queue.Insert(trav);
+        }
+        while(queue.GetSize() !==0){
+            if(trav.left !== null){
+                queue.Insert(trav.left)
+            }
+            if(trav.right !== null){
+                queue.Insert(trav.right)
+            }
+            console.log(trav.value);
+            queue.Remove();
+            if(queue.front === null){
+                break;
+            }
+            trav = queue.front.value;
+        }
+        return;
+    }
 
     PreorderPrint(node){
         if(node === null){
@@ -106,4 +180,4 @@ bst.Insert(64);
 bst.Insert(25);
 bst.Insert(86);
 bst.Insert(212);
-bst.PostorderPrint(bst.root);
+bst.LevelorderPrint(bst.root);
