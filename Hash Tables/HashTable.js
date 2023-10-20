04671a42e06f9f48b8549724fafcf673f80ec4ff
Panos-Stergioulas-Mpolis <panosstergioulasmpolis@gmail.com>
2023-10-20 23:04:49 +0300

    Resize the table after a serten amount of items have been inserted

--- Hash Tables/HashTable.js
+++ Hash Tables/HashTable.js
@@ -11,8 +11,34 @@ function hasFunction(s, tableSize){
 class HashTable{
 
     table = new Array(3);
+    numOfItems = 0;
+
+    resize =()=>{
+        const newTable = new Array(this.table.length * 2);
+        this.table.forEach(item => {
+            if(item){
+                item.forEach(([key, value])=>{
+                    const idx = hasFunction(key, newTable.length)
+                    if(newTable[idx]){
+                        newTable[idx].push([key, value]);
+                    }
+                    else{
+                        newTable[idx] = [[key,value]];
+                    }
+                })
+            }
+        });
+        this.table = newTable;
+    }
 
     setItem = (key, value) =>{
+        this.numOfItems++;
+        const ratio = this.numOfItems / this.table.length;
+
+        if(ratio > 0.8){
+            this.resize();
+        }
+
         const idx = hasFunction(key, this.table.length);
         if(this.table[idx]){
             this.table[idx].push([key, value]);
@@ -34,13 +60,3 @@ class HashTable{
 }
 
 const table = new HashTable();
-
-table.setItem("first name", "bob");
-table.setItem("last name", "fir");
-table.setItem("kids", 4);
-table.setItem("grand kids", 6);
-console.log(table.getItem("first name"));
-console.log(table.getItem("last name"));
-console.log(table.getItem("kids"));
-console.log(table.getItem("grand kids"));
-console.log(table.getItem("age"));
